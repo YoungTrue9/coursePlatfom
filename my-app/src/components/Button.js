fe050c676260ceff1468c555130680e62a41f1b5
Gleb <79607720+YoungTrue9@users.noreply.github.com>
2023-06-16 17:36:09 +0200

    init project

---
+++ my-app/src/components/Button.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import style from './style.css'
+function Button(){
+    return(
+        <div>
+            <button className='btnBlack' style={{marginLeft: '29px'}}>button</button>
+            <button className='btnBlack' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button</button>
+            <button className='btnBlack' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button</button>
+            <button className='btnBlack' style={{width: '140px', height: '64px'}}>button</button>
+
+            <button className='btnWhite'  style={{marginLeft: '29px'}}>button</button>
+            <button className='btnWhite' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button</button>
+            <button className='btnWhite' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button</button>
+            <button className='btnWhite' style={{width: '140px', height: '64px'}}>button</button>
+
+            <button className='btnTanspert'  style={{marginLeft: '29px'}}>button</button>
+            <button className='btnTanspert' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button</button>
+            <button className='btnTanspert' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button</button>
+            <button className='btnTanspert' style={{width: '140px', height: '64px'}}>button</button>
+
+            <button className='btnBlackPlusLeft'  style={{marginLeft: '29px'}}>+ button</button>
+            <button className='btnBlackPlusLeft' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button</button>
+            <button className='btnBlackPlusLeft' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button</button>
+            <button className='btnBlackPlusLeft' style={{width: '140px', height: '64px'}}>+ button</button>
+
+            <button className='btnWhitePlusLeft'  style={{marginLeft: '29px'}}>+ button</button>
+            <button className='btnWhitePlusLeft' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button</button>
+            <button className='btnWhitePlusLeft' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button</button>
+            <button className='btnWhitePlusLeft' style={{width: '140px', height: '64px'}}>+ button</button>
+
+            <button className='btnTanspertPlusLeft'  style={{marginLeft: '29px'}}>+ button</button>
+            <button className='btnTanspertPlusLeft' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button</button>
+            <button className='btnTanspertPlusLeft' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button</button>
+            <button className='btnTanspertPlusLeft' style={{width: '140px', height: '64px'}}>+ button</button>
+
+            <button className='btnBlackPlusRight'  style={{marginLeft: '29px'}}>button +</button>
+            <button className='btnBlackPlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button +</button>
+            <button className='btnBlackPlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button +</button>
+            <button className='btnBlackPlusRight' style={{width: '140px', height: '64px'}}>button +</button>
+
+            <button className='btnWhitePlusRight'  style={{marginLeft: '29px'}}>button +</button>
+            <button className='btnWhitePlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button +</button>
+            <button className='btnWhitePlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button +</button>
+            <button className='btnWhitePlusRight' style={{width: '140px', height: '64px'}}>button +</button>
+
+            <button className='btnTanspertPlusRight'  style={{marginLeft: '29px'}}>button +</button>
+            <button className='btnTanspertPlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>button +</button>
+            <button className='btnTanspertPlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>button +</button>
+            <button className='btnTanspertPlusRight' style={{width: '140px', height: '64px'}}>button +</button>
+
+            <button className='btnBlackTwoPlusRight'  style={{marginLeft: '29px'}}>+ button +</button>
+            <button className='btnBlackTwoPlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button +</button>
+            <button className='btnBlackTwoPlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button +</button>
+            <button className='btnBlackTwoPlusRight' style={{width: '140px', height: '64px'}}>+ button +</button>
+
+            <button className='btnWhiteTwoPlusRight'  style={{marginLeft: '29px'}}>+ button +</button>
+            <button className='btnWhiteTwoPlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button +</button>
+            <button className='btnWhiteTwoPlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button +</button>
+            <button className='btnWhiteTwoPlusRight' style={{width: '140px', height: '64px'}}>+ button +</button>
+
+            <button className='btnTanspertTwoPlusRight'  style={{marginLeft: '29px'}}>button +</button>
+            <button className='btnTanspertTwoPlusRight' style={{width: '99px', height: '48px', marginLeft: '21px'}}>+ button +</button>
+            <button className='btnTanspertTwoPlusRight' style={{width: '118px', height: '56px', marginLeft: '13px'}}>+ button +</button>
+            <button className='btnTanspertTwoPlusRight' style={{width: '140px', height: '64px'}}>+ button +</button>
+
+             <button className='btnBlackPlus'  style={{marginLeft: '16px'}}>+</button>
+             <button className='btnBlackPlus' style={{width: '48px', height: '48px', marginLeft: '11px'}}>+</button>
+             <button className='btnBlackPlus' style={{width: '56px', height: '56px', marginLeft: '6px'}}>+</button>
+             <button className='btnBlackPlus' style={{width: '64px', height: '64px'}}>+</button>
+
+             <button className='btnWhitePlus'  style={{marginLeft: '16px'}}>+</button>
+             <button className='btnWhitePlus' style={{width: '48px', height: '48px', marginLeft: '11px'}}>+</button>
+             <button className='btnWhitePlus' style={{width: '56px', height: '56px', marginLeft: '6px'}}>+</button>
+             <button className='btnWhitePlus' style={{width: '64px', height: '64px'}}>+</button>
+
+             <button className='btnTanspertPlus'  style={{marginLeft: '16px'}}>+</button>
+             <button className='btnTanspertPlus' style={{width: '48px', height: '48px', marginLeft: '11px'}}>+</button>
+             <button className='btnTanspertPlus' style={{width: '56px', height: '56px', marginLeft: '6px'}}>+</button>
+             <button className='btnTanspertPlus' style={{width: '64px', height: '64px'}}>+</button>
+        </div>
+    )
+}
+
+export default Button
